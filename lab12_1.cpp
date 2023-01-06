648466245badf6d40d99b06f741ef0f511166baf
aomsnnn <omsinjah@gmail.com>
2023-01-06 16:07:33 +0700

    lab12.1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,29 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+
+using namespace std;
+
+string g()
+{
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    string x = "";
+    return x = grade[rand()%9];
+}
+
+int main()
+{
+    srand(time(0));
+   
+    
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int j=1;j<=3;j++)
+    {
+        cin.get();
+    }
+    cout << "You will get " << g() << " in this 261102.";
+    
+    return 0;
+
+}
